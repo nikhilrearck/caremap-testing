b5ecc10d0944340c1d5c4260ead4adf3dc1fc993
nikhilrearck <nikhil.khetan@btsavvy.net>
2025-06-17 17:51:33 +0530

    fixed highLevelGoals UI and started working on add your goals

---
+++ app/highLevelGoals/addYourGoal.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function AddYourGoal() {
+  return (
+    <View>
+      <Text>AddYourGoal</Text>
+    </View>
+  );
+}
--- app/highLevelGoals/index.tsx
+++ app/highLevelGoals/index.tsx
@@ -17,7 +17,7 @@ import { CheckIcon } from "@/components/ui/icon";
 import { Feather } from "@expo/vector-icons";
 // import { ProgressBar } from "react-native-paper";
 import { Progress, ProgressFilledTrack } from "@/components/ui/progress";
-import { Center } from "@/components/ui/center";
+import { Link } from "expo-router";
 
 const linkedGoals = [
   "Establish a consistent sleep schedule for better energy and recovery.",
@@ -38,7 +38,7 @@ const userGoals = [
 
 export default function GoalsScreen() {
   return (
-    <SafeAreaView className="flex-1 bg-white px-7 py-12">
+    <SafeAreaView className="flex-1 bg-white px-7 py-8">
       {/* Linked Health System */}
       <Text className="text-lg font-semibold text-cyan-700">
         High level goals (linked Health System)
@@ -52,7 +52,7 @@ export default function GoalsScreen() {
           key={index}
           className=" border border-gray-300 p-3 rounded-md mb-2"
         >
-          <Text className="text-lg text-black">{goal}</Text>
+          <Text className="text-base text-black">{goal}</Text>
         </View>
       ))}
 
@@ -69,44 +69,66 @@ export default function GoalsScreen() {
           key={index}
           className="bg-white border border-gray-300 rounded-md px-3 py-3 mb-2"
         >
-          <View className="flex-row items-start">
-            <Checkbox
-              value="goal-checkbox"
-              size="md"
-              isInvalid={false}
-              isDisabled={false}
-            >
-              <CheckboxIndicator>
-                <CheckboxIcon as={CheckIcon} />
-              </CheckboxIndicator>
-            </Checkbox>
+          <View className="flex-row items-start mb-2">
+            {/* CheckBox */}
+            <View className="flex-row items-center flex-1">
+              <Checkbox
+                value="goal-checkbox"
+                size="md"
+                isInvalid={false}
+                isDisabled={false}
+              >
+                <CheckboxIndicator>
+                  <CheckboxIcon as={CheckIcon} />
+                </CheckboxIndicator>
+              </Checkbox>
 
-            {/* Loose 10kg in 3 months */}
-            <View className="flex-1 ml-2">
-              <Text className="text-lg text-black ">{goal.title}</Text>
-              {goal.daysRemaining && (
-                <Text className="text-sm text-gray-500 mt-1 text-right">
-                  {goal.daysRemaining} days remaining
-                </Text>
-              )}
-              <Center className="w-[280px] h-[20px]">
-                <Progress value={50} size="xs" orientation="horizontal">
-                  <ProgressFilledTrack className="bg-[#08B828]" />
-                </Progress>
-              </Center>
+              {/* Loose 10Kg in 3 months*/}
+              <Text className="text-base text-black ml-3 flex-1">
+                {goal.title}
+              </Text>
             </View>
-
-            <TouchableOpacity className="ml-2">
+            <TouchableOpacity>
               <Feather name="more-vertical" size={20} color="gray" />
             </TouchableOpacity>
           </View>
+
+          <View className="flex-row justify-end">
+            {goal.daysRemaining && (
+              <Text className="text-sm text-gray-500 mr-2">
+                {goal.daysRemaining} days remaining
+              </Text>
+            )}
+          </View>
+
+          {goal.progress && (
+            <View className="w-full px-2 py-2">
+              <Progress
+                value={goal.progress * 100}
+                size="xs"
+                orientation="horizontal"
+              >
+                <ProgressFilledTrack className="bg-[#08B828]" />
+              </Progress>
+            </View>
+          )}
         </View>
       ))}
 
-      {/* Add Button */}
-      <TouchableOpacity className="mt-4 bg-cyan-600 py-3 rounded items-center justify-center">
-        <Text className="text-white font-semibold">Add your goals</Text>
+      {/* Delete button */}
+      <TouchableOpacity className="bg-gray-50 rounded-md px-4 py-2 mt-1 self-end border border-[#49afbe]">
+        <Text className="text-center text-base color-[#49afbe]">Delete</Text>
       </TouchableOpacity>
+
+      {/* Like hr */}
+      <View className="h-px bg-gray-300 my-4" />
+
+      {/* Add Button */}
+      <Link href="/highLevelGoals/addYourGoal" asChild push>
+        <TouchableOpacity className="mt-1 bg-[#49afbe] py-3 rounded flex-row items-center justify-center">
+          <Text className="text-white font-semibold">Add your goals</Text>
+        </TouchableOpacity>
+      </Link>
     </SafeAreaView>
   );
 }
